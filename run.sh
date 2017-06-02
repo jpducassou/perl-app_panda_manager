11e1f44c877218bf3e190fe681ee1c237afb165e
Jean Pierre Ducassou <jpducassou@gmail.com>
2017-06-02 10:29:57 -0300

    Added consul config to run script.

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+CONSUL_HOST="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' node1)"
+CONSUL_PORT=8500
+
 docker run -it --rm \
 	-e "https_proxy=${https_proxy}" \
 	-e "http_proxy=${http_proxy}"   \
+	-e "CONSUL_HOST=${CONSUL_HOST}" \
+	-e "CONSUL_PORT=${CONSUL_PORT}" \
   panda
 
